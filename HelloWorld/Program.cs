05f96b66d9c5f6076eb12183ee3a9c14b69ffa27
ruben gonzalez <rdgonzalez@gafi.com.mx>
2016-10-19 17:13:12 -0500

    Inicial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
